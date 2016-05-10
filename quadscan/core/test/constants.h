b93e2d8d55002569442786f67231ff9f74f1a04e
Mindtraveller <vladimir.penyazkov@gmail.com>
2016-05-10 14:53:04 +0300

    test controller. new version

---
+++ quadscan/core/test/constants.h
@@ -0,0 +1,75 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include "Ultrasonic.h"
+
+/**
+ * Sonars pins
+ */
+#define FRONT_TRIG A5 
+#define FRONT_ECHO A4
+#define RIGHT_TRIG A3
+#define RIGHT_ECHO A2
+#define LEFT_TRIG A1
+#define LEFT_ECHO A0
+#define BACK_TRIG 13
+#define BACK_ECHO 12
+
+/**
+ * Size of package of data
+ */
+#define PACKAGE_SIZE 7
+
+
+const byte THRESHOLD = 150;
+const byte TOO_FAR = 255;
+const byte ONE_STEP = 1;
+
+
+/**
+ * Map cells
+ */
+const byte NOT_SET = 0;
+const byte EMPTY = 1;
+const byte OBSTACLE = 2;
+
+
+enum DIRECTION {
+    FRONT, RIGHT, BACK, LEFT
+};
+
+
+/**
+ * map size
+ */
+const byte BASIC_SIZE = 30;
+
+
+//[100; 250]
+#define THROTTLE 3
+//[190; 250]
+#define ROLL 5
+//[190; 250]
+#define PITCH 6
+//[110; 250]
+#define COLLECTIVE 9
+#define FLIGHT_MODE 10
+//[110; 250]
+#define YAW 11
+
+#define ROLL_MIN 190;
+#define PITCH_MIN 190;
+#define YAW_MIN 110;
+#define COLLECTIVE_MIN 100;
+#define THROTTLE_MIN 100;
+
+#define ROLL_NEUTRAL 220;
+#define PITCH_NEUTRAL 220;
+#define COLLECTIVE_NEUTRAL 180;
+#define YAW_NEUTRAL 180;
+
+#define THROTTLE_INITIAL 105;
+
+#define STEP 1
+
+#endif // CONSTANTS_H
--- quadscan/core/test/test.ino
+++ quadscan/core/test/test.ino
@@ -1,33 +1,36 @@
-//[100; 250]
-#define THROTTLE 3
-//[190; 250]
-#define ROLL 5
-//[190; 250]
-#define PITCH 6
-//[110; 250]
-#define COLLECTIVE 11
-#define FLIGHT_MODE 10
-//[110; 250]
-#define YAW 9
-
-#define ROLL_MIN 190;
-#define PITCH_MIN 190;
-#define YAW_MIN 110;
-#define COLLECTIVE_MIN 100;
-#define THROTTLE_MIN 100;
-
-#define ROLL_NEUTRAL 220;
-#define PITCH_NEUTRAL 220;
-#define COLLECTIVE_NEUTRAL 180;
-#define YAW_NEUTRAL 180;
-
-#define THROTTLE_INITIAL 105;
+#include <SoftwareSerial.h>
+#include "constants.h"
 
 int throttle_value = THROTTLE_INITIAL;
 int roll_value = ROLL_NEUTRAL;
 int pitch_value = PITCH_NEUTRAL;
 int collective_value = COLLECTIVE_NEUTRAL;
 int yaw_value = YAW_NEUTRAL;
+
+byte front_distance = 0;
+byte back_distance = 0;
+byte left_distance = 0;
+byte right_distance = 0;
+
+bool isInitialized = false;
+
+/**
+ * Sonars
+ */
+Ultrasonic Front(FRONT_TRIG, FRONT_ECHO);
+Ultrasonic Right(RIGHT_TRIG, RIGHT_ECHO);
+Ultrasonic Left(LEFT_TRIG, LEFT_ECHO);
+Ultrasonic Back(BACK_TRIG, BACK_ECHO);
+
+byte distance_from(Ultrasonic &sonic)
+{
+    long microsec = sonic.timing();
+    unsigned int result = Front.convert(microsec, Ultrasonic::CM);
+    if (result > THRESHOLD) {
+        return TOO_FAR;
+    }
+    return result;
+}
 
 void setup() {  
   pinMode(THROTTLE, OUTPUT);
@@ -43,87 +46,120 @@ void setup() {
 }
 
 void loop() {
-  while(!Serial.available());
-  
-  char value = Serial.read();
-  switch(value) {
-    case 't':
-      throttle_value -= 10;
-      analogWrite(THROTTLE, throttle_value);
-      break;
-    case 'T':
-      throttle_value += 10;
-      analogWrite(THROTTLE, throttle_value);
-      break;
-    case 'r':
-      roll_value -= 10;
-      analogWrite(ROLL, roll_value);
-      break;
-    case 'R':
-      roll_value += 10;
-      analogWrite(ROLL, roll_value);
-      break;
-    case 'p':
-      pitch_value -= 10;
-      analogWrite(PITCH, pitch_value);
-      break;
-    case 'P':
-      pitch_value += 10;
-      analogWrite(PITCH, pitch_value);
-      break;
-    case 'y':
-      yaw_value -= 10;  
-      analogWrite(YAW, yaw_value);
-      break;
-    case 'Y':
-      yaw_value += 10;  
-      analogWrite(YAW, yaw_value);
-      break;
-    case 'c':
-      collective_value -= 10;  
-      analogWrite(COLLECTIVE, collective_value);
-      break;
-    case 'C':
-      collective_value += 10;  
-      analogWrite(COLLECTIVE, collective_value);
-      break;
-    case 's':
-      throttle_value = THROTTLE_MIN;
-      roll_value = ROLL_MIN;
-      pitch_value = PITCH_MIN;
-      yaw_value = YAW_MIN;
-      collective_value = COLLECTIVE_MIN;
-      analogWrite(THROTTLE, throttle_value);
-      analogWrite(ROLL, roll_value);
-      analogWrite(PITCH, pitch_value);
-      analogWrite(YAW, yaw_value);
-      analogWrite(COLLECTIVE, collective_value);
-      analogWrite(FLIGHT_MODE, 190);
 
-      delay(1000);
-      
-      throttle_value = THROTTLE_INITIAL;
-      roll_value = ROLL_NEUTRAL;
+  if (isInitialized) {
+    front_distance = distance_from(Front);
+    back_distance = distance_from(Back);
+    left_distance = distance_from(Left);
+    right_distance = distance_from(Right);
+
+    if (front_distance < THRESHOLD) {
+      pitch_value += STEP;
+    } else if (back_distance < THRESHOLD) {
+      pitch_value -= STEP;
+    } else {
       pitch_value = PITCH_NEUTRAL;
-      yaw_value = YAW_NEUTRAL;
-      collective_value = COLLECTIVE_NEUTRAL;
-      
-      analogWrite(THROTTLE, throttle_value);
-      analogWrite(ROLL, roll_value);
-      analogWrite(PITCH, pitch_value);
-      analogWrite(YAW, yaw_value);
-      analogWrite(COLLECTIVE, collective_value);
-      analogWrite(FLIGHT_MODE, 200);
-      break;
-    case 'q':
-      throttle_value = THROTTLE_MIN;
-      analogWrite(THROTTLE, throttle_value);
-      break;
+    }
+    
+    if (left_distance < THRESHOLD) {
+      roll_value -= STEP;
+    } else if (right_distance < THRESHOLD) {
+      roll_value += STEP;
+    } else {
+      roll_value = ROLL_NEUTRAL;
+    }
+
+    analogWrite(PITCH, pitch_value);
+    analogWrite(ROLL, roll_value);
   }
+
+  if (Serial.available()) {
+    char value = Serial.read();
+    switch(value) {
+      case 't':
+        throttle_value -= STEP;
+        analogWrite(THROTTLE, throttle_value);
+        break;
+      case 'T':
+        throttle_value += STEP;
+        analogWrite(THROTTLE, throttle_value);
+        break;
+      case 'r':
+        roll_value -= STEP;
+        analogWrite(ROLL, roll_value);
+        break;
+      case 'R':
+        roll_value += STEP;
+        analogWrite(ROLL, roll_value);
+        break;
+      case 'p':
+        pitch_value -= STEP;
+        analogWrite(PITCH, pitch_value);
+        break;
+      case 'P':
+        pitch_value += STEP;
+        analogWrite(PITCH, pitch_value);
+        break;
+      case 'y':
+        yaw_value -= STEP;  
+        analogWrite(YAW, yaw_value);
+        break;
+      case 'Y':
+        yaw_value += STEP;  
+        analogWrite(YAW, yaw_value);
+        break;
+      case 'c':
+        collective_value -= STEP;  
+        analogWrite(COLLECTIVE, collective_value);
+        break;
+      case 'C':
+        collective_value += STEP;  
+        analogWrite(COLLECTIVE, collective_value);
+        break;
+      case 's':
+        isInitialized = true;
+        
+        throttle_value = THROTTLE_MIN;
+        roll_value = ROLL_MIN;
+        pitch_value = PITCH_MIN;
+        yaw_value = YAW_MIN;
+        collective_value = COLLECTIVE_MIN;
+        analogWrite(THROTTLE, throttle_value);
+        analogWrite(ROLL, roll_value);
+        analogWrite(PITCH, pitch_value);
+        analogWrite(YAW, yaw_value);
+        analogWrite(COLLECTIVE, collective_value);
+        analogWrite(FLIGHT_MODE, 190);
   
+        delay(1000);
+        
+        throttle_value = THROTTLE_INITIAL;
+        roll_value = ROLL_NEUTRAL;
+        pitch_value = PITCH_NEUTRAL;
+        yaw_value = YAW_NEUTRAL;
+        collective_value = COLLECTIVE_NEUTRAL;
+        
+        analogWrite(THROTTLE, throttle_value);
+        analogWrite(ROLL, roll_value);
+        analogWrite(PITCH, pitch_value);
+        analogWrite(YAW, yaw_value);
+        analogWrite(COLLECTIVE, collective_value);
+        analogWrite(FLIGHT_MODE, 200);
+        break;
+      case 'q':
+        throttle_value = THROTTLE_MIN;
+        analogWrite(THROTTLE, throttle_value);
+        break;
+    }
+  }
+
+  Serial.print("trottle: ");
   Serial.println(throttle_value);
+  Serial.print("roll: ");
   Serial.println(roll_value);
+  Serial.print("pitch: ");
   Serial.println(pitch_value);
-  Serial.println(yaw_value);
-  Serial.println(collective_value);
+  Serial.println("\n");
+//  Serial.println(yaw_value);
+//  Serial.println(collective_value);
 }
